5806b3332ed969c0f197c86c7e1c689ce93069f1
Contador De Alfonso <2DAW-mconalf2108@IESMARTINEZM.ES>
2026-09-23 11:18:20 +0200

    ejercicios 9y10

--- actividades/js/Ejercicio10.js
+++ actividades/js/Ejercicio10.js
@@ -1,30 +1,23 @@
 {
-"use strict"
-/**10. Programa una función que determine si un número es primo (aquel que solo es divisible por
-sí mismo y 1) o no, pe. miFuncion(7) devolverá true
- */
-let num1;
-do {
-    num1 = Number(prompt("Introduzca un numero: "));
-} while (isNaN(num1));ç
+    "use strict"
+    /**10. Programa una función que determine si un número es primo (aquel que solo es divisible por
+    sí mismo y 1) o no, pe. miFuncion(7) devolverá true
+     */
+    let num;
+    do {
+        num = Number(prompt("Introduzca un numero: "));
+    } while (isNaN(num1));
 
-esPrimo(num1);
+    esPrimo(num1);
 
-
-function esPrimo(num){
-    let esPrimo = false;
-    if(num1%2!=0 && num1%num1==0 && num1%1 == 0){
-        esPrimo==true;
-        console.log("El numero es primo");
-    }else{
-        esPrimo===false;
-        console.log("El numero no es primo"); 
+    function esPrimo(num) {
+        let esPrimo = true;
+        for (let i = 2; i < num; i++) {
+            if (num % i === 0) {
+                esPrimo = false;
+            }
+        }
+        return esPrimo;
     }
-    return esPrimo;
 }
 
-
-
-
-
-}
